f8b76274ff880713418182c829893dcbdc999ee1
Claire Gilligan <feminaprovita@gmail.com>
2019-03-08 20:25:58 -0500

    fixed landing

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -3,6 +3,16 @@ import React from 'react'
 const Landing = () => (
   <div id="landing-text">
     <p>Welcome to clairegilligan.com! Real content coming soon.</p>
+    <p>
+      For now, look me up on{' '}
+      <a href="https://www.linkedin.com/in/claire-gilligan/" target="blank">
+        LinkedIn
+      </a>{' '}
+      and{' '}
+      <a href="https://github.com/feminaprovita/" target="blank">
+        Github
+      </a>.
+    </p>
   </div>
 )
 
