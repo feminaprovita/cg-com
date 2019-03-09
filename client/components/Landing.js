58777bf0b86b86f3a4068f5adc115cb73a99f129
Claire Gilligan <feminaprovita@gmail.com>
2019-03-08 21:00:09 -0500

    photo and links on landing

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,18 +1,39 @@
 import React from 'react'
 
 const Landing = () => (
-  <div id="landing-text">
-    <p>Welcome to clairegilligan.com! Real content coming soon.</p>
-    <p>
-      For now, look me up on{' '}
-      <a href="https://www.linkedin.com/in/claire-gilligan/" target="blank">
-        LinkedIn
-      </a>{' '}
-      and{' '}
-      <a href="https://github.com/feminaprovita/" target="blank">
-        Github
-      </a>.
-    </p>
+  <div id="landing-container">
+    <div id="profile">
+      <img
+        id="profile-pic"
+        src="/profile.png"
+        alt="Claire Gilligan profile picture"
+      />
+    </div>
+    <div id="landing-text">
+      <p>Welcome to clairegilligan.com!</p>
+      <p>
+        You can find me on{' '}
+        <a href="https://www.linkedin.com/in/claire-gilligan/" target="blank">
+          LinkedIn
+        </a>{' '}
+        and{' '}
+        <a href="https://github.com/feminaprovita/" target="blank">
+          Github
+        </a>,<br />
+        or, if you're feeling informal, on{' '}
+        <a href="https://www.facebook.com/feminaprovita/" target="blank">
+          Facebook
+        </a>{' '}
+        and{' '}
+        <a href="https://twitter.com/feminaprovita/" target="blank">
+          Twitter
+        </a>.
+      </p>
+      <p>
+        Check back soon, because I will definitely be adding more stuff here as
+        I go!
+      </p>
+    </div>
   </div>
 )
 
