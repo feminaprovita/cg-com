db05b092867833ae1f1c7ee39a6cd2aef4dbd91a
Claire Gilligan <feminaprovita@gmail.com>
2019-03-08 21:56:37 -0500

    reinstituted component index

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Navbar from './components/Navbar'
-import Landing from './components/Landing'
-// import {Navbar, Landing} from './components'
+// import Navbar from './components/Navbar'
+// import Landing from './components/Landing'
+import {Navbar, Landing} from './components'
 import Routes from './routes'
 
 const App = () => {
