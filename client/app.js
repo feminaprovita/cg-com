0ccc3126ef853ac95ea984f5fe12efbc92da1666
Claire Gilligan <feminaprovita@gmail.com>
2019-03-11 21:20:18 -0400

    changed import-export syntax in components/index

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 // import Navbar from './components/Navbar'
 // import Landing from './components/Landing'
-import {Navbar, Landing} from './components'
+import {Navbar} from './components'
 import Routes from './routes'
 
 const App = () => {
--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,12 @@
+import Landing from './Landing'
+import Navbar from './Navbar'
+
+export {Landing, Navbar}
+
 /**
  * `components/index.js` exists simply as a 'central export' for our components.
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Landing} from './Landing'
-export {default as Navbar} from './Navbar'
+// export {default as Landing} from './Landing'
+// export {default as Navbar} from './Navbar'
